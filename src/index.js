f7f11cbf741585acaf8b79edea77d37eb49d69b7
nickcodesbetter <nicholas.kenyon.may@gmail.com>
2021-08-03 09:28:36 +0100

    added fetch and variable & attempted terminal, but could not get it to work

--- src/index.js
+++ src/index.js
@@ -84,3 +84,22 @@ images.animalCoders.forEach((animalCoder) => {
     //    It took me a while to get array working, realised I was missing a comma between the objects
     // for.each seems to work, now it's just a matter of including the html
     // As I'm writing this, I forgot to do the pseudo code... oops
+
+// I could not get the terminal to work - still says command not found
+
+// I rewatched the tutorial, but could not work out how to get the db.json array and objects displayed.
+
+  const animalCodersPosts = {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(animalCoder)
+  }
+  
+    fetch('http://localhost:3000/images', animalCodersPosts)
+  .then(response => response.json())
+  .then(data => console.log(data));
+  
+  
+  
